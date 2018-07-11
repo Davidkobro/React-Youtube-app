96668b79c2b56d12f81578c67ddb71d1204c7211
David Kobrosky <davidkobrosky@gmail.com>
2018-07-11 17:54:09 -0400

    recent Youtube API add on

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,13 +3,23 @@ import React, { Component } from 'react';
 class SearchBar extends Component{
     constructor(props) {
         super(props);
-
+        // whenever we have state we create new object
+        //when user updates search, we record that change on term
+        //we want term to be the value of the input
+        // only in constructor do we change state unless using setState
         this.state = { term: ''};
     }
-
+// control has value updated by state
     render() { 
-        return <input onChange={event => console.log(event.target.value)} />;
-    }
+        return (
+            <div> 
+                
+                <input 
+                    value = {this.state.term}
+                    onChange={event => this.setState({term: event.target.value})} />
+            </div>
+        );
+   }   
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+const API_KEY = 'AIzaSyA8fO1Vjau20J69gF4maLqfEUAlBt4TxDI';
 
-const App = () => {
+//downwards data flow (most parent component fetch it)
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (data) =>{ 
+            this.setState({ videos: videos });
+        });
+    }
+
+
+   render() {
     return (
     <div>
         <SearchBar />
     </div>
     ); 
+   }
 }
 
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container')); 
