2748aa14103e40af5b48f832e80809a0ac6897ca
David Kobrosky <davidkobrosky@gmail.com>
2018-07-18 13:59:02 -0400

    etag update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyA8fO1Vjau20J69gF4maLqfEUAlBt4TxDI';
 
 //downwards data flow (most parent component fetch it)
@@ -13,8 +14,9 @@ class App extends Component {
 
         this.state = { videos: [] };
 
-        YTSearch({key: API_KEY, term: 'surfboards'}, (data) =>{ 
-            this.setState({ videos: videos });
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) =>{ 
+            this.setState({ videos });
+            //this.setState({videos: videos});
         });
     }
 
@@ -23,6 +25,7 @@ class App extends Component {
     return (
     <div>
         <SearchBar />
+        <VideoList videos={this.state.videos}/>
     </div>
     ); 
    }
